79ed3f5044ccd188adfe9f7e548aba32752394fa
Elizabeth Bodzy <bodze087@newschool.edu>
2019-09-05 22:16:52 -0500

    enter item id and amount you would like to purchase working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,44 +4,54 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-// Your port; if not 3306
-port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-// Your username
-user: "root",
+    // Your username
+    user: "root",
 
-// Your password
-password: "Yosterkitty3",
-database: "bamazon"
+    // Your password
+    password: "Yosterkitty3",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-  });
+    // console.log("connected as id " + connection.threadId + "\n");
+    displayTable();
+});
 
-  var displayTable = function() {
-      connection.query("SELECT * FROM products", function(err,results) {
-          if(err) throw err;
-          console.table(results);
-      })
-  }
+var displayTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.table(res);
+        }
+        buy()
+    })
+    
+};
 
-  var start = function() {
-    connection.query("SELECT * FROM products", function(err,results) {
-        if(err) throw err;
-    inquirer.prompt({
-        name: 'postOrBid',
-        type: 'list',
-        message: 'Would you like to [POST] an auction or [BID] on an auction?',
-        choices: ["POST", "BID"]
-  }).then(function(answer) {
-      if(answer.postOrBid.toUpperCase() == "POST") {
-          postAuction();
-      } else {
-          //bidAuction();
-      }
-  })
-}
+var buy = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([{
+            type: 'input',
+            name: 'item',
+            message: 'What is the item id of the product you would like to purchase?'
+        },
+        {
+            type: 'number',
+            name: 'amount',
+            message: 'How many would you like to purchase?'
+        }
+        ]).then(function (answer) {
+            console.log(answer)
+            
+          
+                
 
-displayTable();
+            
+        })
+    }
+    )}
